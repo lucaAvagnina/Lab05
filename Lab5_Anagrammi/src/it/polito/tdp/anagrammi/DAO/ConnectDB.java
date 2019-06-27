8a8598bec66524d427b5a5457b641e500221f368
Luca Avagnina <lucaavagnina96@gmail.com>
2019-06-27 12:11:15 +0200

    Finito

--- Lab5_Anagrammi/src/it/polito/tdp/anagrammi/DAO/ConnectDB.java
+++ Lab5_Anagrammi/src/it/polito/tdp/anagrammi/DAO/ConnectDB.java
@@ -1,21 +1,32 @@
 package it.polito.tdp.anagrammi.DAO;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 public class ConnectDB {
 
-	static private final String jdbcUrl = "jdbc:mysql://localhost/dizionario?useTimezone=true&serverTimezone=UTC&user=root&password=mylunchisover";
-	static private Connection connection = null;
+	static private final String jdbcUrl = "jdbc:mysql://localhost/dizionario?useTimezone=true&serverTimezone=UTC";
+	static private HikariDataSource ds;
 
 	public static Connection getConnection() {
 
+		if (ds == null) {
+					
+					ds = new HikariDataSource();
+		
+					ds.setJdbcUrl(jdbcUrl);
+					ds.setUsername("root");
+					ds.setPassword("mylunchisover");
+		
+					
+		
+				}
+	
 		try {
-			if (connection == null || connection.isClosed()) {
-				connection = DriverManager.getConnection(jdbcUrl);
-			}
-			return connection;
+			
+			return ds.getConnection();
 
 		} catch (SQLException e) {
 
